b52da7170df68fc386ce6df1bd8ffebd8e8ed438
GD <GD@DESKTOP-F93LCV2>
2017-10-30 16:26:38 +0900

    화면완료

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,21 +1,61 @@
-function readyEvent(){    
-    $(document).on('click', '#menu a', function(event){
-    
-    var url = event.target.href;
-    /* 브라우저 히스토리 내용 담기 */
-    history.pushState(null, null, url);
-    /* 변경 내용 적용 : 링크 시 url 이벤트 처리 */
-    $('#container').load(url + ' #container');
-    /* URL 주소로 이동을 막기 */
-    event.preventDefault();
-    });
-}
+/*
+        $(document).ready(function () {
 
-function popstateEvent(event){
-    /* 변경 내용 적용 : 브라우저 콘트롤 버튼 이벤트 처리 */
-    $('#container').load(location.href + ' #container');
 
-    $(document).ready(readyEvent);
-        
-    $(window).on('popstate', popstateEvent);
-}
+                        var hash = location.hash;
+                        if (hash == "") {
+                            hash = "#home";
+                        }
+                        $("a").on("click", function() {
+                            hash = $(this).attr("href");
+                            htmlLoad();
+                        });
+
+                        function htmlLoad() {
+                            if (hash == "#comm" | hash == "#home" | hash == "#ranking" | hash == "#champ") {
+                                var url = "../html/" + hash.substr(1, hash.length) + ".html";
+                                // div 태그 속에 url에서 가져온 데이터 넣기.
+                                $("#container").load(url);
+
+                                var index;
+                                switch (hash.substr(1, hash.length)) {
+                                    case "home":
+                                        index = 0;
+                                        break;
+                                    case "ranking":
+                                        index = 1;
+                                        break;
+                                    case "comm":
+                                        index = 2;
+                                        break;
+                                    case "champ":
+                                        index = 3;
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                $("a").removeClass("on");
+                                $("a").eq(index).addClass("on");
+                            }
+
+                        }
+
+                        htmlLoad();
+
+                        function popstateEvent(event) {
+                            hash = location.hash;
+                            if (hash == "") {
+                                hash = "#home";
+                            }
+
+
+
+                            htmlLoad();
+
+                        }
+
+
+                        $(window).on('popstate', popstateEvent);
+        });
+*/
